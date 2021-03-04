14bd2b50ab4b4c199d3d8ce076028b37bb2291e2
Vitor de Farias Metzner <contato.vitorfariass@gmail.com>
2021-03-03 22:31:40 -0300

    hellow-node

---
+++ app.js
@@ -0,0 +1,10 @@
+var showSite = true;
+const site = "www.guiadoprogramador.com.br"
+
+console.log("Hello World")
+console.log("My name is Vitor")
+console.log("and I'm learning node.js with the programmer's guide")
+
+if(showSite){
+    console.log(site);
+}
